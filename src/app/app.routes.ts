4ea8c242236fa86e5f11eeb867d2689cde06a741
chengyuxing <chengyuxingo@gmail.com>
2025-06-28 18:54:47 +0800

    简化路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
-  {path: 'main', loadComponent: () => import('./pages/main/main.component').then(c => c.MainComponent)},
   {
     path: 'documents',
     loadChildren: () => import('./pages/documents/documents.routes').then(m => m.routes)
@@ -9,6 +8,6 @@ export const routes: Routes = [
     path: 'guides',
     loadChildren: () => import('./pages/guides/guides.routes').then(m => m.routes)
   },
-  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: '', loadComponent: () => import('./pages/main/main.component').then(c => c.MainComponent)},
   {path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)},
 ];
