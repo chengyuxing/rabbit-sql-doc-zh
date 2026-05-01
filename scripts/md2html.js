033532f1fbc9fe00ad46fc0adb4e08746dd524b3
chengyuxing <chengyuxingo@gmail.com>
2026-05-01 22:30:55 +0800

    update

--- scripts/md2html.js
+++ scripts/md2html.js
@@ -8,14 +8,27 @@ const config = {
   outputDir: `./dist/${pkg.name}`,
 }
 
-const htmlTemplate = body => `<!doctype html>
+const htmlTemplate = (title, body) => `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <meta name="description"
+        content="Rabbit-SQL 是国产极简 SQL 框架，支持动态 SQL 和 Spring Boot，是专为复杂查询而生的超轻量 SQL 框架，适用于对数据安全、审计要求极高的政务项目，可以通过静态 SQL 管理 + 参数化执行，实现最安全可控的数据访问，天然适配国产生态，兼容主流国产数据库（如达梦、人大金仓、OceanBase 等。">
+  <meta name="referrer" content="always">
+  <meta name="author" content="Cheng Yuxing">
+  <meta name="applicable-device" content="pc,mobile">
+  <meta property="og:site_name" content="Rabbit SQL - 极简国产 SQL 框架">
+  <meta property="og:title" content="Rabbit SQL - 极简国产 SQL 框架">
+  <meta property="og:description"
+        content="Rabbit-SQL 是国产极简 SQL 框架，支持动态 SQL 和 Spring Boot，是专为复杂查询而生的超轻量 SQL 框架，适用于对数据安全、审计要求极高的政务项目，可以通过静态 SQL 管理 + 参数化执行，实现最安全可控的数据访问，天然适配国产生态，兼容主流国产数据库（如达梦、人大金仓、OceanBase 等。">
+  <meta property="og:image" content="https://rabbitsql.cn/images/rabbit-sql.svg">
+  <meta property="og:url" content="https://rabbitsql.cn">
+  <meta property="og:type" content="website">
+  <link rel="icon" type="image/svg+xml" href="https://rabbitsql.cn/images/rabbit-sql.svg">
+  <title>Rabbit SQL - ${title}</title>
 </head>
 <body>
 ${body}
@@ -38,7 +51,7 @@ function convertMdFiles(dir) {
 
       const outputName = fullPath.replace(/\.md$/, '.html');
       const outputPath = path.join(config.outputDir, path.relative("public", outputName));
-      fs.writeFileSync(outputPath, htmlTemplate(htmlContent));
+      fs.writeFileSync(outputPath, htmlTemplate(file.name.replace(/\.md$/, ''), htmlContent));
       console.log(`Completed: ${fullPath} --> ${outputPath}`);
     }
   });
